c19eeea9fb9edac4cf2cc2dc6743e56660214f22
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-01-12 21:28:24 -0300

    create MACROS GPIOS aux

--- workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_gpio_driver.h
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_gpio_driver.h
@@ -15,8 +15,8 @@
 
 typedef struct{
 	uint8_t GPIO_PinNumber;
-	uint8_t GPIO_PinMode;
-	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinMode;               /*!< possible values from @GPIO_PIN_MODES >*/
+	uint8_t GPIO_PinSpeed;			    /*!< possible values from @GPIO_PIN_SPEED >*/
 	uint8_t GPIO_PinPuPdControl;
 	uint8_t GPIO_PinOPType;
 	uint8_t GPIO_PinAltFunMode;
@@ -28,11 +28,68 @@ typedef struct{
  */
 
 typedef struct{
-	//pointer to hold the base address of the GPIO peripheral
 	GPIO_RegDef_t *pGPIOx;              /*!< This holds the base addr of the GPIO port to which the pin belongs */
 	GPIO_PinCfg_t GPIO_PinCfg;          /*!< This holds GPIO pin configuration settings */
 
 }GPIO_Handle_t;
+
+/*
+ * @GPIO_PIN_NUMBERS
+ * GPIO pin numbers
+ */
+#define GPIO_PIN_NO_0  			0
+#define GPIO_PIN_NO_1  			1
+#define GPIO_PIN_NO_2  			2
+#define GPIO_PIN_NO_3  			3
+#define GPIO_PIN_NO_4  			4
+#define GPIO_PIN_NO_5  			5
+#define GPIO_PIN_NO_6  			6
+#define GPIO_PIN_NO_7  			7
+#define GPIO_PIN_NO_8  			8
+#define GPIO_PIN_NO_9  			9
+#define GPIO_PIN_NO_10  		10
+#define GPIO_PIN_NO_11 			11
+#define GPIO_PIN_NO_12  		12
+#define GPIO_PIN_NO_13 			13
+#define GPIO_PIN_NO_14 			14
+#define GPIO_PIN_NO_15 			15
+
+/*
+ * @GPIO_PIN_MODES
+ * GPIO pin possible modes
+ */
+#define GPIO_MODE_IN 			0
+#define GPIO_MODE_OUT 			1
+#define GPIO_MODE_ALTFN 		2
+#define GPIO_MODE_ANALOG 		3
+#define GPIO_MODE_IT_FT     	4
+#define GPIO_MODE_IT_RT     	5
+#define GPIO_MODE_IT_RFT    	6
+
+
+/*
+ * GPIO pin possible output types
+ */
+#define GPIO_OP_TYPE_PP   		0
+#define GPIO_OP_TYPE_OD   		1
+
+
+/*
+ * @GPIO_PIN_SPEED
+ * GPIO pin possible output speeds
+ */
+#define GPIO_SPEED_LOW			0
+#define GPIO_SPEED_MEDIUM		1
+#define GPIO_SPEED_FAST			2
+#define GPOI_SPEED_HIGH			3
+
+
+/*
+ * GPIO pin pull up AND pull down configuration macros
+ */
+#define GPIO_NO_PUPD   			0
+#define GPIO_PIN_PU				1
+#define GPIO_PIN_PD				2
 
 /*****************************************************************
  *				APIs supported by this driver                    *
