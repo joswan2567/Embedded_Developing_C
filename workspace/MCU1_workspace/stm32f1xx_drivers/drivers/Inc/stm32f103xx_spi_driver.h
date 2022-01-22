9987690c801431bed5e4559a5c3660f3909de70c
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-01-22 20:42:19 -0300

    create SPI's driver file

---
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_spi_driver.h
@@ -0,0 +1,37 @@
+/*
+ * stm32f1xx_spi_driver.h
+ *
+ *  Created on: Jan 22, 2022
+ *      Author: José Wanderson
+ */
+
+#ifndef INC_STM32F103XX_SPI_DRIVER_H_
+#define INC_STM32F103XX_SPI_DRIVER_H_
+
+#include "stm32f103xx.h"
+/*
+ * This is a Configuration Structure for a SPIx peripheral
+ */
+
+typedef struct{
+	uint8_t SPI_DeviceMode;           /*!<possible modes for device> */
+	uint8_t SPI_BusCfg;               /*!<possible types for communication>*/
+	uint8_t SPI_SclkSpeed;			  /*!<possible speed clock>*/
+	uint8_t SPI_DFF;				  /*!<possible size data 8 or 16 bits >*/
+	uint8_t SPI_CPOL;
+	uint8_t SPI_CPHA;
+	uint8_t SPI_SSM;				  /*!<select slave for software or hardware >*/
+
+}SPI_Cfg_t;
+
+/*
+ * This is a Handle structure for a SPIx peripheral
+ */
+
+typedef struct{
+	SPI_RegDef_t *pSPIx;              /*!< This holds the base addr of the SPIx port to which the pin belongs */
+	SPI_Cfg_t     SPI_Cfg;			  /*!< This holds SPIx configuration settings */
+
+}SPI_Handle_t;
+
+#endif /* INC_STM32F103XX_SPI_DRIVER_H_ */
---
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Src/stm32f103xx_spi_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32f103xx_spi_driver.c
+ *
+ *  Created on: Jan 22, 2022
+ *      Author: José Wanderson
+ */
+
+
