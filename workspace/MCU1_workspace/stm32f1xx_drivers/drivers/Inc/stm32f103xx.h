6201b4e846e3ea4e8776b6b5cc6eaa731c0673d1
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-01-23 13:39:16 -0300

    create MACRO bit position SPI_CR1

--- workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx.h
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx.h
@@ -383,6 +383,19 @@ typedef struct{
 #define GPIO_PIN_SET		SET
 #define GPIO_PIN_RESET  	RESET
 
+
+/*
+ * Bits position definitions of SPI peripheral
+ */
+#define SPI_CR1_CPHA		0
+#define SPI_CR1_CPOL		1
+#define SPI_CR1_MSTR		2
+#define SPI_CR1_BR			3
+#define SPI_CR1_SSM			9
+#define SPI_CR1_RXONLY		10
+#define SPI_CR1_DFF			11
+#define SPI_CR1_BIDIMODE	15
+
 #include "stm32f103xx_gpio_driver.h"
 #include "stm32f103xx_spi_driver.h"
 
