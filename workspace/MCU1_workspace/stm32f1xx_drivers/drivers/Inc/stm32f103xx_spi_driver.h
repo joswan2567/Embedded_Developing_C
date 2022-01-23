8b2bdf2d4565588c5f3c4aee790fb2b34b42cbe2
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-01-23 02:22:35 -0300

    create scope of functions's driver

--- workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_spi_driver.h
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_spi_driver.h
@@ -9,10 +9,10 @@
 #define INC_STM32F103XX_SPI_DRIVER_H_
 
 #include "stm32f103xx.h"
+
 /*
  * This is a Configuration Structure for a SPIx peripheral
  */
-
 typedef struct{
 	uint8_t SPI_DeviceMode;           /*!<possible modes for device> */
 	uint8_t SPI_BusCfg;               /*!<possible types for communication>*/
@@ -27,11 +27,45 @@ typedef struct{
 /*
  * This is a Handle structure for a SPIx peripheral
  */
-
 typedef struct{
 	SPI_RegDef_t *pSPIx;              /*!< This holds the base addr of the SPIx port to which the pin belongs */
 	SPI_Cfg_t     SPI_Cfg;			  /*!< This holds SPIx configuration settings */
 
 }SPI_Handle_t;
+
+
+/*****************************************************************
+ *				 APIs supported by this driver                   *
+ *  For more info. about the APIs check the function definition  *
+ *****************************************************************/
+
+/*
+ * Peripheral Clock Setup
+ */
+void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnOrDi);
+
+/*
+ * Init and DeInit
+ */
+void SPI_Init(SPI_Handle_t *pSPIHandle);
+void SPI_DeInit(SPI_RegDef_t *pSPIx);
+
+/*
+ * Data send and receive
+ */
+void SPI_Send(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Size);
+void SPI_Receive(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t Size);
+
+/*
+ * IRQ configuration and ISR handling
+ */
+void SPI_IRQITCfg(uint8_t IRQNumber, uint8_t EnOrDi);
+void SPI_IRQPriorityCfg(uint8_t IRQNumber, uint8_t IRQPriority);
+void SPI_IRQHandling(SPI_Handle_t *pHandle);
+
+/*
+ * Other Peripheral Control API's
+ */
+
 
 #endif /* INC_STM32F103XX_SPI_DRIVER_H_ */
