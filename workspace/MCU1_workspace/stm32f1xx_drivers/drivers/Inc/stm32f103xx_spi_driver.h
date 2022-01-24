35114d27939d29d21d86a36bba205a59ccaefa01
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-01-24 20:36:28 -0300

    create MACROS status flags SPI

--- workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_spi_driver.h
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_spi_driver.h
@@ -82,6 +82,14 @@ typedef struct{
 #define SPI_SSM_EN								1
 #define SPI_SSM_DI								0
 
+/*
+ * SPI related status flags definitions
+ */
+#define SPI_TXE_FLAG							(1 << SPI_SR_TXE)
+#define SPI_RXNE_FLAG							(1 << SPI_SR_RXNE)
+#define SPI_BUSY_FLAG							(1 << SPI_SR_BSY)
+
+
 /*****************************************************************
  *				 APIs supported by this driver                   *
  *  For more info. about the APIs check the function definition  *
