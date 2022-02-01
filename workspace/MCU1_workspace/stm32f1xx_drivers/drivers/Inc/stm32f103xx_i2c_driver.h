6918f795068fc570c5b631362260829862954f83
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-01-31 23:52:00 -0300

    create prototypes functions I2C driver

--- workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_i2c_driver.h
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_i2c_driver.h
@@ -49,4 +49,42 @@ typedef struct{
 #define I2C_FM_DUTY_2			0
 #define I2C_FM_DUTY_16_9		1
 
+/*****************************************************************
+ *				 APIs supported by this driver                   *
+ *  For more info. about the APIs check the function definition  *
+ *****************************************************************/
+
+/*
+ * Peripheral Clock Setup
+ */
+void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi);
+
+/*
+ * Init and DeInit
+ */
+void I2C_Init(I2C_Handle_t *pI2CHandle);
+void I2C_DeInit(I2C_RegDef_t *pI2Cx);
+
+/*
+ * Data send and receive
+ */
+
+
+/*
+ * IRQ configuration and ISR handling
+ */
+void I2C_IRQITCfg(uint8_t IRQNumber, uint8_t EnOrDi);
+void I2C_IRQPriorityCfg(uint8_t IRQNumber, uint8_t IRQPriority);
+
+/*
+ * Other Peripheral Control API's
+ */
+void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi);
+uint8_t I2C_GetFlagStatus(I2C_RegDef_t *pI2Cx, uint32_t FlagName);
+
+/*
+ * Application Callback
+ */
+void I2C_AppEventCallback(I2C_Handle_t *pI2CHandle, uint8_t AppEv);
+
 #endif /* INC_STM32F103XX_I2C_DRIVER_H_ */
