aa8e5e8e1b34f879ca6395c3fb18b6af737c29e3
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-01-31 23:38:32 -0300

    create MACROS for user I2C cfg

--- workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_i2c_driver.h
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_i2c_driver.h
@@ -7,16 +7,17 @@
 
 #ifndef INC_STM32F103XX_I2C_DRIVER_H_
 #define INC_STM32F103XX_I2C_DRIVER_H_
+
 #include "stm32f103xx.h"
 
 /*
  * This is a Configuration Structure for a I2Cx peripheral
  */
 typedef struct{
-	uint8_t I2C_SCLSpeed;             /*!<possible speed for SCL > */
-	uint8_t SPI_DeviceAddr;           /*!<(if device mode slave) device addr >*/
-	uint8_t SPI_ACKControl;			  /*!<Control ACK>*/
-	uint8_t SPI_FMDutyCycle;		  /*!<TODO: >*/
+	uint32_t I2C_SCLSpeed;             /*!<possible speed for SCL > */
+	uint8_t I2C_DeviceAddr;           /*!<(if device mode slave) device addr >*/
+	uint8_t I2C_ACKControl;			  /*!<Control ACK>*/
+	uint8_t I2C_FMDutyCycle;		  /*!<TODO: >*/
 
 }I2C_Cfg_t;
 
@@ -28,5 +29,24 @@ typedef struct{
 	I2C_Cfg_t	 I2C_Cfg;
 
 }I2C_Handle_t;
+
+/*
+ * @I2C_SCLSpeed
+ */
+#define I2C_SCL_SPEED_SM		100000
+#define I2C_SCL_SPEED_FM4K		400000
+#define I2C_SCL_SPEED_FM2K		200000
+
+/*
+ * @I2C_ACKControl
+ */
+#define I2C_ACK_EN				1
+#define I2C_ACK_DI				0
+
+/*
+ * @I2C_FMDutyCycle
+ */
+#define I2C_FM_DUTY_2			0
+#define I2C_FM_DUTY_16_9		1
 
 #endif /* INC_STM32F103XX_I2C_DRIVER_H_ */
