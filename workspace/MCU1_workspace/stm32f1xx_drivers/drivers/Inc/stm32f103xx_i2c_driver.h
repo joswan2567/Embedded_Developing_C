747b8640ccd91979aee6764ec1121a19e30d772a
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-01-31 22:51:34 -0300

    init I2C driver

---
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_i2c_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f103xx_i2c_driver.h
+ *
+ *  Created on: Jan 31, 2022
+ *      Author: José Wanderson
+ */
+
+#ifndef INC_STM32F103XX_I2C_DRIVER_H_
+#define INC_STM32F103XX_I2C_DRIVER_H_
+
+
+
+#endif /* INC_STM32F103XX_I2C_DRIVER_H_ */
---
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Src/stm32f103xx_i2c_driver.c
@@ -0,0 +1,9 @@
+/*
+ * stm32f103xx_i2c_driver.c
+ *
+ *  Created on: Jan 31, 2022
+ *      Author: José Wanderson
+ */
+
+#include "stm32f103xx_i2c_driver.h"
+
