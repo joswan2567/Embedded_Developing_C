e2230086a2008fce988b84669f7525674772706f
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-02-01 16:25:59 -0300

    create I2C_Init

--- workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Src/stm32f103xx_i2c_driver.c
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Src/stm32f103xx_i2c_driver.c
@@ -7,6 +7,10 @@
 
 #include "stm32f103xx_i2c_driver.h"
 
+uint16_t AHB_PreScaler[8] = {2, 4, 8, 16, 32, 64, 128, 256, 512};
+uint16_t APB1_PreScaler[4] = {2, 4, 8, 16};
+
+
 // This is a weak implementation, the application may override this function.
 __weak void I2C_AppEventCallback(I2C_Handle_t *pI2CHandle, uint8_t AppEv){}
 
@@ -34,6 +38,50 @@ void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi){
 	else if (pI2Cx == I2C2) I2C2_PCLK_DI();
 }
 
+uint32_t RCC_GetPLLOutputClock(){
+	return 0;
+}
+
+uint32_t RCC_GetPCLK1Value(void){
+	uint32_t pclk1, SysClk;
+	uint8_t clksrc, temp, ahbp, apb1p;
+
+	clksrc = ((RCC->CFGR >> 2) & 0x03);
+
+	if(clksrc == 0){
+		SysClk = 16000000;
+	}
+	else if(clksrc == 1){
+		SysClk = 8000000;
+	}
+	else if(clksrc == 2){
+		SysClk = RCC_GetPLLOutputClock();
+	}
+
+	// for AHB PreScaler
+	temp = ((RCC->CFGR >> 4) & 0x0F);
+
+	if(temp < 8){
+		ahbp = 1;
+	}
+	else{
+		ahbp = AHB_PreScaler[temp - 8];
+	}
+
+	// for APB1 PreScaler
+	temp = ((RCC->CFGR >> 10) & 0x07);
+
+	if(temp < 4){
+		apb1p = 1;
+	}
+	else{
+		apb1p = APB1_PreScaler[temp - 4];
+	}
+
+	pclk1 = (SysClk * apb1p) / ahbp; // (SysClk / ahbp) / apb1p;
+
+	return pclk1;
+}
 /*********************************************************************
 * @fn      		  	 - I2C_Init
 *
@@ -46,7 +94,43 @@ void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi){
 * @Note              - none
 */
 void I2C_Init(I2C_Handle_t *pI2CHandle){
+	uint32_t tempreg = 0;
 
+	// ack control
+	tempreg |= pI2CHandle->I2C_Cfg.I2C_ACKControl << 10;
+	pI2CHandle->pI2Cx->CR1 = tempreg;
+
+	// cfg the FREQ field of CR2
+	tempreg = 0;
+	tempreg |= RCC_GetPCLK1Value() / 1000000U;
+	pI2CHandle->pI2Cx->CR2 = (tempreg & 0x03F); // isso deixa somente o 5 bits primeiros bit visiveis
+
+	//program the device own addr
+	tempreg |= pI2CHandle->I2C_Cfg.I2C_DeviceAddr << 1;
+	tempreg |= (1 << 14);
+	pI2CHandle->pI2Cx->OAR1 = tempreg;
+
+	//CCR calculations
+	uint16_t ccr_value = 0;
+	tempreg = 0;
+	if(pI2CHandle->I2C_Cfg.I2C_SCLSpeed <= I2C_SCL_SPEED_SM){
+		// mode is standard mode
+		ccr_value = RCC_GetPCLK1Value() / (2 * pI2CHandle->I2C_Cfg.I2C_SCLSpeed);
+		tempreg |=(ccr_value & 0xFFF); // aqui deixamos visiveis somente os 12 primeiros bits
+	}
+	else{
+		// mode is fast mode
+		tempreg |= (1 << 15);
+		tempreg |= (pI2CHandle->I2C_Cfg.I2C_FMDutyCycle << 14);
+		if(pI2CHandle->I2C_Cfg.I2C_FMDutyCycle == I2C_FM_DUTY_2){
+			ccr_value = RCC_GetPCLK1Value() / (3 * pI2CHandle->I2C_Cfg.I2C_SCLSpeed);
+		}
+		else{
+			ccr_value = RCC_GetPCLK1Value() / (25 * pI2CHandle->I2C_Cfg.I2C_SCLSpeed);
+		}
+		tempreg |=(ccr_value & 0xFFF);
+	}
+	pI2CHandle->pI2Cx->CCR = tempreg;
 }
 
 /*********************************************************************
