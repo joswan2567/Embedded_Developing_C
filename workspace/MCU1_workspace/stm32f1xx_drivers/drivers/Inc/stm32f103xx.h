aecb3747ae5df06aebd399523ca44bf160c42c22
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-02-01 16:33:10 -0300

    create reset register I2Cx

--- workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx.h
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx.h
@@ -315,6 +315,11 @@ typedef struct{
 #define I2C2_PCLK_DI()             ( RCC->APB1ENR &= ~( 1 << 22 ) )            /*!<  I2C2 clock disabled */
 
 /*
+ *  Macros to reset I2Cx peripherals
+ */
+#define I2C1_REG_RESET()			do{ (RCC->APB1RSTR |= (1 << 21)); (RCC->APB1RSTR &= ~(1 << 21)); }while(0)
+#define I2C2_REG_RESET()            do{ (RCC->APB1RSTR |= (1 << 22)); (RCC->APB1RSTR &= ~(1 << 22)); }while(0)
+/*
  * Clock Enable Macros for SPIx peripherals
  */
 #define SPI1_PCLK_EN()             ( RCC->APB2ENR |= ( 1 << 12 ) )             /*!<  SPI1 clock enabled */
