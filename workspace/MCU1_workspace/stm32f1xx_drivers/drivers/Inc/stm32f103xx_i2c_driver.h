00952a1354c9f59acd53c90a2aff9190df61cb62
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-02-02 00:46:29 -0300

    create MACROS for I2C status flag def

--- workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_i2c_driver.h
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_i2c_driver.h
@@ -49,6 +49,25 @@ typedef struct{
 #define I2C_FM_DUTY_2			0
 #define I2C_FM_DUTY_16_9		1
 
+/*
+ * I2C related status flags definitions
+ */
+#define I2C_TXE_FLAG		(1 << I2C_SR1_TXE)
+#define I2C_RXNE_FLAG		(1 << I2C_SR1_RXNE)
+#define I2C_SB_FLAG			(1 << I2C_SR1_SB)
+#define I2C_FLAG_OVR  		( 1 << I2C_SR1_OVR)
+#define I2C_FLAG_AF   		( 1 << I2C_SR1_AF)
+#define I2C_FLAG_ARLO 		( 1 << I2C_SR1_ARLO)
+#define I2C_FLAG_BERR 		( 1 << I2C_SR1_BERR)
+#define I2C_FLAG_STOPF 		( 1 << I2C_SR1_STOPF)
+#define I2C_FLAG_ADD10 		( 1 << I2C_SR1_ADD10)
+#define I2C_FLAG_BTF  		( 1 << I2C_SR1_BTF)
+#define I2C_FLAG_ADDR 		( 1 << I2C_SR1_ADDR)
+#define I2C_FLAG_TIMEOUT 	( 1 << I2C_SR1_TIMEOUT)
+
+#define I2C_DISABLE_SR  	RESET
+#define I2C_ENABLE_SR   	SET
+
 /*****************************************************************
  *				 APIs supported by this driver                   *
  *  For more info. about the APIs check the function definition  *
@@ -68,7 +87,7 @@ void I2C_DeInit(I2C_RegDef_t *pI2Cx);
 /*
  * Data send and receive
  */
-
+void I2C_MasterSendData(I2C_Handle_t *pI2CHandle, uint8_t *pTxBuffer, uint8_t Size, uint8_t SlaveAddr);
 
 /*
  * IRQ configuration and ISR handling
