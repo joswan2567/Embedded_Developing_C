2ebe9dbd8f17524db0c1c6da287f5f98ea974cd8
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-02-02 00:47:11 -0300

    create MACROS for I2C app events

--- workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_i2c_driver.h
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_i2c_driver.h
@@ -68,6 +68,19 @@ typedef struct{
 #define I2C_DISABLE_SR  	RESET
 #define I2C_ENABLE_SR   	SET
 
+/*
+ * I2C application events macros
+ */
+#define I2C_EV_TX_CMPLT  	 	0
+#define I2C_EV_RX_CMPLT  	 	1
+#define I2C_EV_STOP       		2
+#define I2C_ERROR_BERR 	 		3
+#define I2C_ERROR_ARLO  		4
+#define I2C_ERROR_AF    		5
+#define I2C_ERROR_OVR   		6
+#define I2C_ERROR_TIMEOUT 		7
+#define I2C_EV_DATA_REQ         8
+#define I2C_EV_DATA_RCV         9
 /*****************************************************************
  *				 APIs supported by this driver                   *
  *  For more info. about the APIs check the function definition  *
