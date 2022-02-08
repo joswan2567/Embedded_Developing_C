ae9712441fee9ed8feeff5d70f2bdfdcb7f793e9
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-02-07 23:57:27 -0300

    Update stm32f103xx_i2c_driver.h

--- workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_i2c_driver.h
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_i2c_driver.h
@@ -137,6 +137,9 @@ void I2C_ER_IRQHandling(I2C_Handle_t *pI2CHandle);
  */
 void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi);
 uint8_t I2C_GetFlagStatus(I2C_RegDef_t *pI2Cx, uint32_t FlagName);
+void I2C_GenerateStopCondition(I2C_RegDef_t *pI2Cx);
+
+void I2C_SlaveEnableDisableCallbackEvents(I2C_RegDef_t *pI2Cx,uint8_t EnorDi);
 
 /*
  * Application Callback
