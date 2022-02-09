adc233a9f7f5faf45e3218d7a8a63518ad552d63
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-02-09 18:19:09 -0300

    create file for usart driver

---
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx_usart_driver.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f103xx_usart_driver.h
+ *
+ *  Created on: Feb 9, 2022
+ *      Author: José Wanderson
+ */
+
+#ifndef INC_STM32F103XX_USART_DRIVER_H_
+#define INC_STM32F103XX_USART_DRIVER_H_
+
+
+
+#endif /* INC_STM32F103XX_USART_DRIVER_H_ */
---
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Src/stm32f103xx_usart_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32f103xx_usart_driver.c
+ *
+ *  Created on: Feb 9, 2022
+ *      Author: José Wanderson
+ */
+
+
