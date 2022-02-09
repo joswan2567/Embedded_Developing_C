226a7d02e63bc34818cdfb5336b6bf058cc3ee81
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-02-09 18:52:36 -0300

    add Bits position definitions of USART peripheral

--- workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx.h
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Inc/stm32f103xx.h
@@ -534,6 +534,64 @@ typedef struct{
 #define I2C_CCR_DUTY 			14
 #define I2C_CCR_FS  			15
 
+/*
+ * Bits position definitions of USART peripheral
+ */
+#define USART_SR_PE				0
+#define USART_SR_FE				1
+#define USART_SR_NE				2
+#define USART_SR_ORE			3
+#define USART_SR_IDLE			4
+#define USART_SR_RXNE			5
+#define USART_SR_TC				6
+#define USART_SR_TXE			7
+#define USART_SR_LBD			8
+#define USART_SR_CTS			9
+
+#define USART_DR				0
+
+#define USART_BRR_DIV_FRAC		0
+#define USART_BRR_DIV_MANT		4
+
+#define USART_CR1_SBK			0
+#define USART_CR1_RWU			1
+#define USART_CR1_RE			2
+#define USART_CR1_TE			3
+#define USART_CR1_IDLEIE		4
+#define USART_CR1_RXNEIE		5
+#define USART_CR1_TCIE			6
+#define USART_CR1_TXEIE			7
+#define USART_CR1_PEIE			8
+#define USART_CR1_PS			9
+#define USART_CR1_PCE			10
+#define USART_CR1_WAKE			11
+#define USART_CR1_M				12
+#define USART_CR1_UE			13
+
+#define USART_CR2_ADD			0
+#define USART_CR2_LBDL			5
+#define USART_CR2_LBCL			8
+#define USART_CR2_CPHA			9
+#define USART_CR2_CPOL			10
+#define USART_CR2_CLKEN			11
+#define USART_CR2_STOP			12
+#define USART_CR2_LINEN			14
+
+#define USART_CR3_EIE			0
+#define USART_CR3_IREN			1
+#define USART_CR3_IRLP			2
+#define USART_CR3_HDSEL			3
+#define USART_CR3_NACK			4
+#define USART_CR3_SCEN			5
+#define USART_CR3_DMAR			6
+#define USART_CR3_DMAT			7
+#define USART_CR3_RTSE			8
+#define USART_CR3_CTSE			9
+#define USART_CR3_CTSIE			10
+
+#define USART_GTPR_PSC			0
+#define USART_GTPR_GT			8
+
 #include "stm32f103xx_gpio_driver.h"
 #include "stm32f103xx_spi_driver.h"
 #include "stm32f103xx_i2c_driver.h"
