35928771aa8631ba3c7891c8db37d00a0b419b31
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-02-10 15:13:45 -0300

    create function USART_Init

--- workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Src/stm32f103xx_usart_driver.c
+++ workspace/MCU1_workspace/stm32f1xx_drivers/drivers/Src/stm32f103xx_usart_driver.c
@@ -93,6 +93,105 @@ void USART_ClearFlag(USART_RegDef_t *pUSARTx, uint8_t StatusFlagName){
 }
 
 /*********************************************************************
+ * @fn      		  - USART_Init
+ *
+ * @brief             -
+ *
+ * @param[in]         -
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -
+ *
+ * @Note              - Resolve all the TODOs
+
+ */
+void USART_Init(USART_Handle_t *pUSARTHandle){
+
+	//Temporary variable
+	uint32_t tempreg=0;
+
+/******************************** Configuration of CR1******************************************/
+
+	//Implement the code to enable the Clock for given USART peripheral
+	USART_PeriClockControl(pUSARTHandle->pUSARTx, ENABLE);
+
+	//Enable USART Tx and Rx engines according to the USART_Mode configuration item
+	if ( pUSARTHandle->USART_Cfg.USART_Mode == USART_MODE_ONLY_RX){
+		//Implement the code to enable the Receiver bit field
+		tempreg |= ( 1 << USART_CR1_RE);
+	}else if (pUSARTHandle->USART_Cfg.USART_Mode == USART_MODE_ONLY_TX){
+		//Implement the code to enable the Transmitter bit field
+		tempreg |= ( 1 << USART_CR1_TE );
+
+	}else if (pUSARTHandle->USART_Cfg.USART_Mode == USART_MODE_TXRX){
+		//Implement the code to enable the both Transmitter and Receiver bit fields
+		tempreg |= ( ( 1 << USART_CR1_RE) | ( 1 << USART_CR1_TE) );
+	}
+
+    //Implement the code to configure the Word length configuration item
+	tempreg |= pUSARTHandle->USART_Cfg.USART_WordLength << USART_CR1_M;
+
+
+    //Configuration of parity control bit fields
+	if ( pUSARTHandle->USART_Cfg.USART_ParityControl == USART_PARITY_EN_EVEN){
+		//Implement the code to enale the parity control
+		tempreg |= ( 1 << USART_CR1_PCE);
+
+		//Implement the code to enable EVEN parity
+		//Not required because by default EVEN parity will be selected once you enable the parity control
+
+	}else if (pUSARTHandle->USART_Cfg.USART_ParityControl == USART_PARITY_EN_ODD ){
+		//Implement the code to enable the parity control
+	    tempreg |= ( 1 << USART_CR1_PCE);
+
+	    //Implement the code to enable ODD parity
+	    tempreg |= ( 1 << USART_CR1_PS);
+
+	}
+
+   //Program the CR1 register
+	pUSARTHandle->pUSARTx->CR1 = tempreg;
+
+/******************************** Configuration of CR2******************************************/
+
+	tempreg=0;
+
+	//Implement the code to configure the number of stop bits inserted during USART frame transmission
+	tempreg |= pUSARTHandle->USART_Cfg.USART_NoOfStopBits << USART_CR2_STOP;
+
+	//Program the CR2 register
+	pUSARTHandle->pUSARTx->CR2 = tempreg;
+
+/******************************** Configuration of CR3******************************************/
+
+	tempreg=0;
+
+	//Configuration of USART hardware flow control
+	if ( pUSARTHandle->USART_Cfg.USART_HWFlowControl == USART_HW_FLOW_CTRL_CTS){
+		//Implement the code to enable CTS flow control
+		tempreg |= ( 1 << USART_CR3_CTSE);
+
+
+	}else if (pUSARTHandle->USART_Cfg.USART_HWFlowControl == USART_HW_FLOW_CTRL_RTS){
+		//Implement the code to enable RTS flow control
+		tempreg |= (1 << USART_CR3_RTSE);
+
+	}else if (pUSARTHandle->USART_Cfg.USART_HWFlowControl == USART_HW_FLOW_CTRL_CTS_RTS){
+		//Implement the code to enable both CTS and RTS Flow control
+		tempreg |= ( (1 << USART_CR3_CTSE) | (1 << USART_CR3_RTSE) );
+	}
+
+	pUSARTHandle->pUSARTx->CR3 = tempreg;
+
+/******************************** Configuration of BRR(Baudrate register)******************************************/
+
+	//Implement the code to configure the baud rate
+	//We will cover this in the lecture. No action required here
+
+}
+
+/*********************************************************************
  * @fn      		  - USART_IRQInterruptCfg
  *
  * @brief             - This function clear status of flag
