adf82cd22d23dc57adf5c9739fb4a5699e620a13
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-03-06 14:37:11 -0300

    create task_handler

---
+++ workspace/RTOS_workspace/005Menu/Core/Inc/task_handler.h
@@ -0,0 +1,13 @@
+/*
+ * task_handler.h
+ *
+ *  Created on: Mar 6, 2022
+ *      Author: José Wanderson
+ */
+
+#ifndef INC_TASK_HANDLER_H_
+#define INC_TASK_HANDLER_H_
+
+
+
+#endif /* INC_TASK_HANDLER_H_ */
---
+++ workspace/RTOS_workspace/005Menu/Core/Src/task_handler.c
@@ -0,0 +1,8 @@
+/*
+ * task_handler.c
+ *
+ *  Created on: Mar 6, 2022
+ *      Author: José Wanderson
+ */
+
+#include "task_handler.h"
