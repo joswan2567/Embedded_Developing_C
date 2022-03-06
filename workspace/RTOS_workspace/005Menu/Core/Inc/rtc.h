46ce602d0b3e43fed5042da496ab685b05b17100
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-03-06 14:37:02 -0300

    create rtc

---
+++ workspace/RTOS_workspace/005Menu/Core/Inc/rtc.h
@@ -0,0 +1,13 @@
+/*
+ * rtc.h
+ *
+ *  Created on: Mar 6, 2022
+ *      Author: José Wanderson
+ */
+
+#ifndef INC_RTC_H_
+#define INC_RTC_H_
+
+
+
+#endif /* INC_RTC_H_ */
---
+++ workspace/RTOS_workspace/005Menu/Core/Src/rtc.c
@@ -0,0 +1,9 @@
+/*
+ * rtc.c
+ *
+ *  Created on: Mar 6, 2022
+ *      Author: José Wanderson
+ */
+
+
+#include "rtc.h"
