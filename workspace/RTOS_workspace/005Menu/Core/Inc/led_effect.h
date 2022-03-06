9ca5f2f84ac1d672dde98dfffd5792dd361c6cec
José Wanderson <55920014+joswan2567@users.noreply.github.com>
2022-03-06 14:36:53 -0300

    create led_effect

---
+++ workspace/RTOS_workspace/005Menu/Core/Inc/led_effect.h
@@ -0,0 +1,13 @@
+/*
+ * led_effect.h
+ *
+ *  Created on: Mar 6, 2022
+ *      Author: José Wanderson
+ */
+
+#ifndef INC_LED_EFFECT_H_
+#define INC_LED_EFFECT_H_
+
+
+
+#endif /* INC_LED_EFFECT_H_ */
---
+++ workspace/RTOS_workspace/005Menu/Core/Src/led_effect.c
@@ -0,0 +1,9 @@
+/*
+ * led_effect.c
+ *
+ *  Created on: Mar 6, 2022
+ *      Author: José Wanderson
+ */
+
+#include "led_effect.h"
+
